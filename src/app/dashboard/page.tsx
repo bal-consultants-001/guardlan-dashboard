9fab3af8886ee6135586d8804a23bb3963fcb2d5
Ben Lane <balcomputers19@gmail.com>
2025-05-27 10:59:23 +0100

    Update dashboard view

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -93,7 +93,7 @@ export default function DashboardPage() {
 		  .from('logs')
 		  .select('*')
 		  .in('UniID', taIDs)
-		logsData = logsResponse.data
+		logsData = logsResponse.data ?? []
 		logsError = logsResponse.error
 		console.log('Logs data:', logsData, 'Logs error:', logsError)
 	  } else {
