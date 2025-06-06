7959fb7c83de667302b111164b03e262e8ad013a
Ben Lane <balcomputers19@gmail.com>
2025-06-06 09:21:03 +0100

    Justify home page text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,7 +48,7 @@ export default function HomePage() {
       </div>
 
       <div className="py-20 text-center">
-		<div ClassName="max-w-x1 mx-auto">
+		<div className="max-w-x1 mx-auto">
 			<h1 className="text-5xl font-bold mb-4">Protect Your Home from Ads</h1>
 			<p className="text-lg mb-4">
 			  Our hardware AdBlocker GuardLAN secures your whole network from intrusive ads and trackers.
