370f542ba0482092926d0e0abc4d93e4032bb623
Ben Lane <balcomputers19@gmail.com>
2025-06-11 15:43:19 +0100

    Update pages

--- src/app/support/page.tsx
+++ src/app/support/page.tsx
@@ -15,6 +15,7 @@ import { useRouter } from 'next/navigation'
 import Link from "next/link";
 import type { User } from '@supabase/supabase-js'
 import { supabase } from '@/lib/supabase';
+import Layout from '@/components/Layout'
 
 export default function DashboardPage() {
   const router = useRouter()
@@ -60,8 +61,8 @@ export default function DashboardPage() {
   if (!user) return <p>Loading tickets...</p>
 
   return (
-    <main className="p-6 max-w-5xl mx-auto space-y-12">
-	  <section className="py-10 px-4 text-center">
+    <Layout>
+	  <section className="bg-[linear-gradient(to_right,var(--color-red1),var(--color-purple2),var(--color-blue2))] w-full py-4 overflow-hidden">
 		<div className="py-8 grid md:grid-cols-3 gap-6 float-right">
 			<Link href="/" className="inline-block bg-white text-black px-6 py-3 rounded-lg font-semibold hover:bg-gray-800">
 					Back to Home
@@ -110,6 +111,6 @@ export default function DashboardPage() {
           Raise a New Ticket
         </button>
       </section>
-    </main>
+    </Layout>
   )
 }
