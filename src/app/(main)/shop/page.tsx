c96d990670520409798a7937f836515ae332a977
Ben Lane <balcomputers19@gmail.com>
2025-06-12 15:07:27 +0100

    Updates

--- src/app/(main)/shop/page.tsx
+++ src/app/(main)/shop/page.tsx
@@ -99,7 +99,8 @@ export default function ShopPage() {
       <section className="bg-[linear-gradient(to_right,var(--color-red1),var(--color-purple2),var(--color-blue2))] w-full py-4 overflow-hidden">
         <div className="flex justify-end gap-4 px-6">
           {user ? (
-            <Link href="/dashboard" className="bg-black text-white px-6 py-3 rounded-lg font-semibold hover:bg-gray-800">Dashboard</Link>
+            <Link href="/" className="bg-black text-white px-6 py-3 rounded-lg font-semibold hover:bg-gray-800">Home</Link>
+			<Link href="/dashboard" className="bg-black text-white px-6 py-3 rounded-lg font-semibold hover:bg-gray-800">Dashboard</Link>
           ) : (
             <>
               <Link href="/register" className="bg-black text-white px-6 py-3 rounded-lg font-semibold hover:bg-gray-800">Register</Link>
