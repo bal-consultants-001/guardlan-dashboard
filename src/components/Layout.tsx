090bade9e55b17086579f1523351e84da399e42f
Ben Lane <balcomputers19@gmail.com>
2025-06-16 14:10:42 +0100

    Update auth

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -28,7 +28,6 @@ export default function Layout({ children }: LayoutProps) {
 
   const handleLogout = async () => {
     await supabase.auth.signOut();
-	setUser(null);
     router.push('/');
   };
 
