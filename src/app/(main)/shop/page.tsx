d7890f2f8c80d5471173305e6e5e6dd4d767dea8
Ben Lane <balcomputers19@gmail.com>
2025-07-01 21:13:51 +0100

    Update shop page

--- src/app/(main)/shop/page.tsx
+++ src/app/(main)/shop/page.tsx
@@ -141,7 +141,10 @@ export default function ShopPage() {
 
       {/* Postcode Check */}
       <section className="p-6 bg-gray-50 text-center">
-        <h2 className="text-xl font-semibold mb-2">Check if we service your area</h2>
+        <h2 className="text-xl font-semibold mb-2">Is our service available in your area?</h2>
+		<p className="text-lg mb-4">
+		  Before attempting to purchase one of our devices, please use the Postcode checker to confirm we are operating in your area.
+		</p>
         <div className="flex justify-center gap-2 flex-wrap mb-3">
           <input
             className="border border-gray-300 p-2 w-64 rounded"
