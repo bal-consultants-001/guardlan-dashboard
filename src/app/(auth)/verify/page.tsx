cf763832cb2847b89d66bd506887b1d533a7c1a2
Ben Lane <balcomputers19@gmail.com>
2025-07-13 17:24:28 +0100

    Add verify page

--- src/app/(auth)/verify/page.tsx
+++ src/app/(auth)/verify/page.tsx
@@ -8,32 +8,35 @@ import { useRouter } from 'next/navigation'
 export default function SuccessPage() {
   const router = useRouter()
   const [seconds, setSeconds] = useState(5)
-		
+
+  useEffect(() => {
     if (seconds === 0) {
       router.push('/login')
       return
     }
 
-    const timer = setTimeout(() => setSeconds((s) => s - 1), 3000)
+    const timer = setTimeout(() => setSeconds((s) => s - 1), 1000)
     return () => clearTimeout(timer)
   }, [seconds, router])
 
   return (
     <main className="min-h-screen flex items-center justify-center bg-gray-50 px-4">
-		<section className="bg-white text-black rounded-2xl shadow-lg p-8 w-full max-w-7/10 items-center text-center">
-		  <h1 className="text-3xl font-bold text-green-600 mb-4">Please verify your email:</h1>
-		  <p className="mb-4">
-			Thank you for registering, you will be able to login once you have verified your email address.
-			You will automatically be redirected to the Login page in{' '}
-			<span className="font-semibold">{seconds}</span> second{seconds !== 1 && 's'}...
-		  </p>
-		  <button
-			onClick={() => router.push('/login')}
-			className="bg-black text-white px-4 py-2 rounded hover:bg-gray-800"
-		  >
-			Go to Login Now
-		  </button>
-	    </section>
+      <section className="bg-white text-black rounded-2xl shadow-lg p-8 w-full max-w-md text-center">
+        <h1 className="text-3xl font-bold text-green-600 mb-4">
+          Please verify your email:
+        </h1>
+        <p className="mb-4">
+          Thank you for registering. You will be able to log in once you have verified your email
+          address. You will automatically be redirected to the Login page in{' '}
+          <span className="font-semibold">{seconds}</span> second{seconds !== 1 && 's'}...
+        </p>
+        <button
+          onClick={() => router.push('/login')}
+          className="bg-black text-white px-4 py-2 rounded hover:bg-gray-800"
+        >
+          Go to Login Now
+        </button>
+      </section>
     </main>
   )
 }
